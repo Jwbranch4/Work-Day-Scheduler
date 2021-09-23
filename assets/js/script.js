6a05bfbd4915d5d75983d48496999df147037de3
Jwbranch4 <87912530+Jwbranch4@users.noreply.github.com>
2021-09-23 13:14:49 -0500

    added color time var to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,74 @@
 var day = moment().format("dddd, MMMM Do YYYY");
+var currentTime = moment().format("hh:mm");
+console.log(currentTime);
 
 $("#currentDay").html(day);
+
+function colorTime() {
+  var hour = moment().hour();
+  if (hour < 9) {
+    $("#9am").addClass("future");
+  } else if (hour === 9) {
+    $("#9am").addClass("present");
+  } else if (hour > 9) {
+    $("#9am").addClass("past");
+  }
+  if (hour < 10) {
+    $("#10am").addClass("future");
+  } else if (hour === 10) {
+    $("#10am").addClass("present");
+  } else if (hour > 10) {
+    $("#10am").addClass("past");
+  }
+  if (hour < 11) {
+    $("#11am").addClass("future");
+  } else if (hour === 11) {
+    $("#11am").addClass("present");
+  } else if (hour > 11) {
+    $("#11am").addClass("past");
+  }
+  if (hour < 12) {
+    $("#12am").addClass("future");
+  } else if (hour === 12) {
+    $("#12am").addClass("present");
+  } else if (hour > 12) {
+    $("#12pm").addClass("past");
+  }
+  if (hour < 13) {
+    $("#1pm").addClass("future");
+  } else if (hour === 13) {
+    $("#1pm").addClass("present");
+  } else if (hour > 13) {
+    $("#1pm").addClass("past");
+  }
+  if (hour < 14) {
+    $("#2pm").addClass("future");
+  } else if (hour === 14) {
+    $("#2pm").addClass("present");
+  } else if (hour > 14) {
+    $("#2pm").addClass("past");
+  }
+  if (hour < 15) {
+    $("#3pm").addClass("future");
+  } else if (hour === 15) {
+    $("#3pm").addClass("present");
+  } else if (hour > 15) {
+    $("#3pm").addClass("past");
+  }
+  if (hour < 16) {
+    $("#4pm").addClass("future");
+  } else if (hour === 16) {
+    $("#4pm").addClass("present");
+  } else if (hour > 16) {
+    $("#4pm").addClass("past");
+  }
+  if (hour < 17) {
+    $("#5pm").addClass("future");
+  } else if (hour === 17) {
+    $("#5pm").addClass("present");
+  } else if (hour > 17) {
+    $("#5pm").addClass("past");
+  }
+}
+
+colorTime();
